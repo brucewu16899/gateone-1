a05a3e96d63543d4b1351d703ca835a269a0dd19
saulo alves <saulo@mail.aflitos.net>
2014-05-22 23:02:43 +0200

    loopback

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-docker run -d --name gateone -p 0.0.0.0:443:443 hansd/gateone gateone --origins="https://data.aflitos.net"
+docker run -d --name gateone -p 127.0.0.1:443:443 hansd/gateone gateone --origins="127.0.0.1;https://data.aflitos.net"
