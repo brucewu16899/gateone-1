d5e6771afba59a7196bf9f26b866e7ac31c0d53b
Saulo Alves <sauloal@gmail.com>
2014-09-11 00:07:00 +0200

    right data folder

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 docker stop gateone; docker rm gateone
 
-SRC=$HOME/data/gateone
+SRC=$HOME/data/docker/gateone
 docker run -d --name gateone -v $SRC/data -p 127.0.1.1:8002:443 hansd/gateone gateone --origins="http://127.0.0.1:8001;http://127.0.1.1:8001;http://data.aflitos.net:8001" --disable_ssl
 
 #watch docker logs gateone
